0de6467b85b152331385837e6cd52fe8744a96db
BaeKyeongMan <b991212@naver.com>
2026-01-16 21:44:08 +0900

    (2026-01-16) 책상 시스템

--- Program/source.c
+++ Program/source.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "source.h"
 
-#define SIZE 100000
+void ImageLoad(const char* fileName);
 
 int main()
 {
@@ -25,31 +27,8 @@ int main()
 
 #pragma region 파일 읽기
 
-	FILE* file = fopen("data.txt", "r");
-
-	int count = 0;
-
-	char character = NULL;
-
-	while ((character = fgetc(file)) != EOF)
-	{
-		count++;
-	}
-
-	printf("count : %d\n", count);
-
-	char buffer[SIZE] = { 0, };
-
-	// 첫 번째 매개변수 : 읽은 데이터를 저장할 메모리 버퍼의 포인터 변수
-	// 두 번째 매개변수 : 각 데이터 항목의 크기 
-	// 세 번째 매개변수 : 데이터를 읽어올 항목의 수
-	// 네 번째 매개변수 : 데이터를 읽어올 파일의 포인터 변수
-
-	fread(buffer, sizeof(char), sizeof(buffer), file);
-
-	printf("%s", buffer);
-
-	fclose(file);
+	ImageLoad("Resources/Unit.txt");
+	ImageLoad("Resources/Object.txt");
 
 #pragma endregion
 
@@ -58,4 +37,36 @@ int main()
 
 
 	return 0;
+}
+
+void ImageLoad(const char* fileName)
+{
+	FILE* file = fopen(fileName, "r");
+
+	int count = 0;
+
+	int character = '\0';
+
+	while ((character = fgetc(file)) != EOF)
+	{
+		count++;
+	}
+
+	rewind(file);
+
+	char* buffer = malloc(count + 1);
+
+	buffer[count] = '\0';
+
+
+	// 첫 번째 매개변수 : 읽은 데이터를 저장할 메모리 버퍼의 포인터 변수
+	// 두 번째 매개변수 : 각 데이터 항목의 크기 
+	// 세 번째 매개변수 : 데이터를 읽어올 항목의 수
+	// 네 번째 매개변수 : 데이터를 읽어올 파일의 포인터 변수
+
+	fread(buffer, sizeof(char), count, file);
+
+	printf("%s", buffer);
+
+	fclose(file);
 }
